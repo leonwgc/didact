d9d657c5a7abc653635e07c81a83e889a75b638b
leonwgc <guochao.wang@dianrong.com>
2019-08-20 17:12:30 +0800

    queue update

--- myreact/component.js
+++ myreact/component.js
@@ -1,13 +1,19 @@
 import {reconcile} from './render';
+import queueUpdate from './queueUpdate';
 
 export default class Component {
 	constructor(props) {
 		this.props = props;
 		this.state = this.state || {};
+		this.__dirty = false;
+		this.forceUpdate = this.forceUpdate.bind(this);
 	}
 
 	setState(partialState) {
 		this.state = Object.assign({}, this.state, partialState);
+		queueUpdate(this);
+	}
+	forceUpdate() {
 		reconcile(this.instance.dom.parentNode, this.instance, this.instance.element);
 	}
 }
---
+++ myreact/queueUpdate.js
@@ -0,0 +1,18 @@
+const nextTick = Promise.resolve().then.bind(Promise.resolve());
+const queue = [];
+
+export default function queueUpdate(c) {
+	if (!c.__dirty) {
+		c.__dirty = true;
+		queue.push(c);
+		nextTick(process);
+	}
+}
+
+function process() {
+	while (queue.length) {
+		var c = queue.shift();
+		c.forceUpdate();
+		c.__dirty = false;
+	}
+}
